987b5d0feee2e9b45c5123cf73a02f6514858d04
Rodrigo de Sarasqueta <78835285+rotpando@users.noreply.github.com>
2023-01-19 10:06:45 -0300

    sql

--- workconnect/workconnect/settings.py
+++ workconnect/workconnect/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'workconnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Work_connect',
+        'HOST': 'database-work-connect.cf23tejqrk8l.eu-west-3.rds.amazonaws.com',
+        'USER': 'postgres',
+        'PASSWORD': 'Bakkali12!',
+        'POST': '5432'
+
     }
 }
 
